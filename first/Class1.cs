34e3bea6be7a967600bbc71ef67ee8c85d48374c
Ahmed hamza <ahmedhamza7895123@gmail.com>
2021-07-06 02:01:49 +0200

    Add project files.

---
+++ first/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace first
+{
+    static public class  Gpa
+    {
+        public static int Grade { get; set; }
+        public static int RestigerdHours { get; set; }
+        public static int RestigerdHours { get; set; }
+        public static int RestigerdHours { get; set; }
+
+    }
+}
---
+++ first/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace first
+{
+    class  Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            float points=0f;
+            int totalHours=0;
+            float gpapoint = 0f;
+            for (int d = 0; d < 12; d+=5)
+            {
+                
+
+            }
+            
+
+            for (int i = 0; i < 7; i++,gpapoint+=5) 
+
+            {
+                Console.WriteLine("enter your course's grade ");
+                int grade = Convert.ToInt32(Console.ReadLine());
+
+                if (grade < 60)
+                {
+                    gpapoint = 0;
+                }
+
+                Console.WriteLine("enter your course's grade ");
+                
+                Console.WriteLine("enter course's hours ");
+                int hours = Convert.ToInt32(Console.ReadLine());
+                
+                points += grade * hours;
+                totalHours += hours;
+
+
+
+            }
+            float gpa = points / totalHours;
+            Console.WriteLine("your Gpa = {0}",gpa);
+            
+        }
+    }
+}
