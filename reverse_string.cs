31a8a6fe8a1f1199b7a4468c09c892e96e860673
Coreleone1972 <86518389+Coreleone1972@users.noreply.github.com>
2021-07-09 15:11:16 +0200

    Add files via upload

---
+++ reverse_string.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+
+namespace EdabitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 123;
+            StringBuilder s = new StringBuilder();
+            s.Append("ashjx");
+            Console.WriteLine(s);
+            
+            
+            Console.WriteLine(reverse(x));
+            
+
+
+        }
+        static string reverse(int i) 
+        {
+            
+            string s = i.ToString();
+
+            char[] car = s.ToCharArray();
+            StringBuilder builder = new StringBuilder();
+            for (int x = car.Length-1; x >-1; x--)
+            {
+                builder.Append(car[x]);
+            }
+            //add the int as a string in builder 
+            builder.Append(s.ToString());
+
+            s = builder.ToString();
+            
+
+            return s ;
+            
+            
+        
+        
+        
+        }
+
+        
+
+
+
+    }
+}
